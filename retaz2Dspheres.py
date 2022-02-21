4b6166bc394f8f24c7ab03a61bc8450d459e63f1
unknown <andrej.paulen@gmail.com>
2022-02-21 16:28:38 +0100

    testujem dalsiu zmenu

--- retaz2Dspheres.py
+++ retaz2Dspheres.py
@@ -20,7 +20,7 @@ import time, datetime
 
 #ahoj tt je zmena
 
-#pridavam do new br
+#pridavam do new br, testujem dalsiu zmenu
 
 def distance(a,b):
     return np.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2 + (a[2]-b[2])**2)
